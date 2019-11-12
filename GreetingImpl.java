a598bd5d9ce111505ee3554e92706f72390a68b0
Parvez Ahamad Shaik <parvezahamadsk@gmail.com>
2019-11-12 23:55:00 +0530

    Create GreetingImpl.java

---
+++ GreetingImpl.java
@@ -0,0 +1,10 @@
+package com.parvez.junit;
+
+public class GreetingImpl implements Greeting {
+    @Override
+    public String greet(String name) {
+        if(name==null || name.length ()==0)
+            throw new IllegalArgumentException (  );
+        return "Hello "+name;
+    }
+}
